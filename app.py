45a510b146f566a926e34edd3dbcdda594eeff1d
Flora2020 <rossignols1989@gmail.com>
2021-08-19 22:48:23 +0800

    feat: connect to MySQL

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+import os
 from flask import Flask
+from common.database import db
+from dotenv import load_dotenv
 
+if os.environ.get('FLASK_ENV', '') != 'production':
+    load_dotenv()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
 
 @app.route('/')
@@ -10,4 +17,5 @@ def home():
 
 
 if __name__ == '__main__':
+    db.init_app(app)
     app.run()
