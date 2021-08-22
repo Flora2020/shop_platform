77f5ed01bccc123f41c68386fbe2c37dcb06c0db
Flora2020 <rossignols1989@gmail.com>
2021-08-22 16:37:50 +0800

    feat: add Product model

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,3 @@
 from models.user import User
 from models.category import Category
+from models.product import Product
---
+++ models/product.py
@@ -0,0 +1,26 @@
+from app import db
+from datetime import datetime
+
+
+class Product(db.Model):
+    __tablename__ = 'product'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    image_url = db.Column(db.String(100))
+    inventory = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(2000))
+    insert_time = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    update_time = db.Column(db.DateTime, onupdate=datetime.now, nullable=False, default=datetime.now)
+
+    def __repr__(self):
+        return f'<Product {self.email}>'
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
