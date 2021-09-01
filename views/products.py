29cde3d4db2f415e940d518d80548c6d70cd4539
Flora2020 <rossignols1989@gmail.com>
2021-09-02 01:25:08 +0800

    feat: show a porduct

--- views/products.py
+++ views/products.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for, request, render_template
+from models import Product
+from common.utils import pretty_date
 
 
 product_blueprint = Blueprint('products', __name__)
@@ -8,3 +10,16 @@ product_blueprint = Blueprint('products', __name__)
 def get_products():
     return redirect(url_for('home'))
 
+
+@product_blueprint.route('/<string:product_id>', methods=['GET'])
+def get_product(product_id):
+    if request.method == 'GET':
+        if not product_id.isnumeric():
+            return redirect(url_for('home'))
+
+        product = Product.find_by_id(product_id)
+        if product:
+            product.update_time = pretty_date(product.update_time)
+            return render_template('/products/product.html', product=product)
+        else:
+            return redirect(url_for('home'))
