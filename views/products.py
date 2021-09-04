c756170dae9926bee31ea2b94abea8d34df1938c
Flora2020 <rossignols1989@gmail.com>
2021-09-04 16:28:58 +0800

    feat: user can fill in form to create product

--- views/products.py
+++ views/products.py
@@ -2,6 +2,7 @@ from flask import Blueprint, redirect, url_for, request, render_template, flash
 from models import Product
 from common.utils import pretty_date
 from common.flash_message import product_not_found, seller_products_not_found
+from common.forms import NewProduct
 
 
 product_blueprint = Blueprint('products', __name__)
@@ -41,3 +42,11 @@ def get_seller_products(seller_id):
     else:
         flash(*seller_products_not_found)
         return redirect(url_for('home'))
+
+
+@product_blueprint.route('/new', methods=['GET', 'POST'])
+def new_product():
+    form = NewProduct()
+    if form.validate_on_submit():
+        return 'success'
+    return render_template('products/new.html', form=form)
