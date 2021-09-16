bcb57175c9ea1f975864b9c2c00a5a5fd7c55cd3
Flora2020 <rossignols1989@gmail.com>
2021-09-16 23:40:16 +0800

    refactor: query optimization

--- common/constant.py
+++ common/constant.py
@@ -2,7 +2,6 @@
 USER = 'user'
 CART_ID = 'cart_id'
 CART_ITEMS = 'cart_items'
-PRODUCT = 'product'
 PRODUCT_ID = 'product_id'
 QUANTITY = 'quantity'
 INSERT_TIME = 'insert_time'
--- views/carts.py
+++ views/carts.py
@@ -5,22 +5,31 @@ from models import Product, Cart, CartItem
 from common.previous_page import previous_page
 from common.utils import find_one_dictionary_in_list
 from common.flash_message import product_not_found, cart_is_empty
-from common.constant import USER, CART_ID, CART_ITEMS, PRODUCT, PRODUCT_ID, QUANTITY, INSERT_TIME, UPDATE_TIME
+from common.constant import USER, CART_ID, CART_ITEMS, PRODUCT_ID, QUANTITY, INSERT_TIME, UPDATE_TIME
 
 cart_blueprint = Blueprint('carts', __name__)
 
 
 @cart_blueprint.route('/')
 def get_cart_items():
-    # TODO: Query optimization
     cart_items = None
 
     if session.get(USER) and session.get(USER).get(CART_ID):
-        cart_items = CartItem.find_by_cart_id(session[USER][CART_ID])
+        cart_items = CartItem.query\
+            .with_entities(CartItem.quantity, Product.name, Product.price, Product.image_url)\
+            .join(Product)\
+            .filter(CartItem.cart_id == session.get(USER).get(CART_ID))\
+            .filter(CartItem.product_id == Product.id).all()
 
     elif session.get(CART_ITEMS):
         for item in session[CART_ITEMS]:
-            item[PRODUCT] = Product.find_by_id(item[PRODUCT_ID])
+            product = Product.query\
+                .with_entities(Product.name, Product.price, Product.image_url)\
+                .filter(Product.id == item[PRODUCT_ID])\
+                .first()
+            item['name'] = product.name
+            item['price'] = product.price
+            item['image_url'] = product.image_url
         cart_items = session[CART_ITEMS]
 
     if not cart_items:
