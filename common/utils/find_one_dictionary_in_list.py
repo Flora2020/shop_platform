a3c2e2efe6abf93fd350d5c45836bbd3dfc26693
Flora2020 <rossignols1989@gmail.com>
2021-09-16 23:42:18 +0800

    fix: should return item, but return None

--- common/utils/find_one_dictionary_in_list.py
+++ common/utils/find_one_dictionary_in_list.py
@@ -5,5 +5,4 @@ def find_one_dictionary_in_list(array: List[Dict], key: str, value: Any) -> Unio
     for item in array:
         if item.get(key) == value:
             return item
-        else:
-            return None
+    return None
