9301b0f9220c7e25604bf50018fa61b93b35aee7
Flora2020 <rossignols1989@gmail.com>
2021-09-29 13:26:27 +0800

    feat: send parameters to NewebPay (2): TradeInfo

--- helpers/orders_view_helper/get_payment_data.py
+++ helpers/orders_view_helper/get_payment_data.py
@@ -2,6 +2,13 @@ import os
 from datetime import datetime
 from typing import Dict, Union
 
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+
+HashKey = os.environ.get('HashKey')
+HashIV = os.environ.get('HashIV')
+URL = os.environ.get('URL')
+
 
 def query_string_encode(data: Dict) -> str:
     key_value_pairs = []
@@ -11,8 +18,15 @@ def query_string_encode(data: Dict) -> str:
     return '&'.join(key_value_pairs)
 
 
+def aes_encrypt(data: str, key: bytes = HashKey.encode('ascii'), iv: bytes = HashIV.encode('ascii')) -> str:
+    # 藍新金流Newebpay_MPG串接手冊_MPG_1.1.1 page 65
+    # aes-256-cbc
+    cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+    ciphered_data = cipher.encrypt(pad(data.encode('utf8'), block_size=32))
+    return ciphered_data.hex()
+
+
 def get_trade_info(order_id: Union[str, int], amount: Union[str, int], email: str) -> str:
-    URL = os.environ.get('URL')
     data = {
         'MerchantID': os.environ.get('MerchantID'),
         'RespondType': 'JSON',
@@ -30,4 +44,4 @@ def get_trade_info(order_id: Union[str, int], amount: Union[str, int], email: st
     }
 
     trade_info = query_string_encode(data)
-    return trade_info
+    return aes_encrypt(trade_info)
