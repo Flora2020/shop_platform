355ae53f61eafff24c3c356de701944a527abc39
Flora2020 <rossignols1989@gmail.com>
2021-10-28 15:15:59 +0800

    refactor: prevent testing two functions in a test

--- tests/test_aes_encrypt.py
+++ tests/test_aes_encrypt.py
@@ -1,22 +1,14 @@
-from helpers.orders_view_helper.get_payment_data import query_string_encode, aes_encrypt
+from dotenv import load_dotenv
+load_dotenv()  # aes_encrypt needs environ variables
 
-# 藍新金流Newebpay_MPG串接手冊_MPG_1.1.1 page 65-66 範例資料
 
-data = {
-    'MerchantID': 3430112,
-    'RespondType': 'JSON',
-    'TimeStamp': 1485232229,
-    'Version': 1.4,
-    'MerchantOrderNo': 'S_1485232229',
-    'Amt': 40,
-    'ItemDesc': 'UnitTest'
-}
-Key = '12345678901234567890123456789012'.encode('ascii')
-IV = '1234567890123456'.encode('ascii')
-expect_output = 'ff91c8aa01379e4de621a44e5f11f72e4d25bdb1a18242db6cef9ef07d80b0165e476fd1d9acaa53170272c82d122961e1a0700a7427cfa1cf90db7f6d6593bbc93102a4d4b9b66d9974c13c31a7ab4bba1d4e0790f0cbbbd7ad64c6d3c8012a601ceaa808bff70f94a8efa5a4f984b9d41304ffd879612177c622f75f4214fa'
-result = aes_encrypt(query_string_encode(data), Key, IV)
+def test_aes_encrypt():
+    from helpers.orders_view_helper.get_payment_data import aes_encrypt
 
-if result != expect_output:
-    print('not pass')
-else:
-    print('pass')
+    # 藍新金流Newebpay_MPG串接手冊_MPG_1.1.1 page 65-66 範例資料
+    data = 'MerchantID=3430112&RespondType=JSON&TimeStamp=1485232229&Version=1.4&MerchantOrderNo=S_1485232229&Amt=40&ItemDesc=UnitTest'
+    Key = '12345678901234567890123456789012'.encode('ascii')
+    IV = '1234567890123456'.encode('ascii')
+    expect_output = 'ff91c8aa01379e4de621a44e5f11f72e4d25bdb1a18242db6cef9ef07d80b0165e476fd1d9acaa53170272c82d122961e1a0700a7427cfa1cf90db7f6d6593bbc93102a4d4b9b66d9974c13c31a7ab4bba1d4e0790f0cbbbd7ad64c6d3c8012a601ceaa808bff70f94a8efa5a4f984b9d41304ffd879612177c622f75f4214fa'
+
+    assert aes_encrypt(data, Key, IV) == expect_output
---
+++ tests/test_query_string_encode.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+load_dotenv()  # get_payment_data.py needs environ variables
+
+
+def test_query_string_encode():
+    from helpers.orders_view_helper.get_payment_data import query_string_encode
+
+    data = {
+        'MerchantID': 3430112,
+        'RespondType': 'JSON',
+        'TimeStamp': 1485232229,
+        'Version': 1.4,
+        'MerchantOrderNo': 'S_1485232229',
+        'Amt': 40,
+        'ItemDesc': 'UnitTest'
+    }
+    expect_output = 'MerchantID=3430112&RespondType=JSON&TimeStamp=1485232229&Version=1.4&MerchantOrderNo=S_1485232229&Amt=40&ItemDesc=UnitTest'
+
+    assert query_string_encode(data) == expect_output
